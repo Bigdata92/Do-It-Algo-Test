4ecc5fbb70fc1f36827fd9ebe3883acc75bc2678
Bigdata92 <wltkqdl@naver.com>
2023-07-18 21:57:21 +0900

    Q002

---
+++ problems/Q002.py
@@ -0,0 +1,8 @@
+num = int(input())
+list1 = list(map(int, input().split()))
+print(list1)
+print(max(list1))
+tmp1 = max(list1)
+list2 = [i/tmp1 * 100 for i in list1]
+print(list2)
+print(sum(list2) / len(list2))

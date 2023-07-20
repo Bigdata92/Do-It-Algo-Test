7041fa1826d9be8833366094f2e8f4264aa3b59e
Bigdata92 <wltkqdl@naver.com>
2023-07-20 14:52:12 +0900

    시간초과

--- problems/Q004.py
+++ problems/Q004.py
@@ -11,18 +11,20 @@ li1 = []
 for _ in range(n):
     li1.append(list(map(int, input().split())))
 
-print(li1)
-
 li1_sum = [[0] for _ in range(n+1)]
-print(li1_sum)
 
 for i in range(n):
     for j in range(n):
         li1_sum[i+1].append(li1_sum[i+1][-1] + li1[i][j])
 
-print(li1_sum)
-#
-# for _ in range(m):
-#     x1, y1, x2, y2 = map(int, input().split())
-#     for idx in range(x1, x2+1):
-#         print(li1_sum[idx - 1][y2] - li1_sum[idx - 1][y1 - 1])
+sum1 = 0
+sum2 = 0
+for _ in range(m):
+    x1, y1, x2, y2 = map(int, input().split())
+    result = 0
+    for idx1 in range(x1, x2+1):
+        sum1 = li1_sum[idx1][y2]
+        sum2 = li1_sum[idx1][y1-1]
+        result += (sum1 - sum2)
+
+    print(result)

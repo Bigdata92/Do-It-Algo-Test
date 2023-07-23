154a8e981a9824b6b5556238b379c58a8c379897
JiseopSong <46867559+Bigdata92@users.noreply.github.com>
2023-07-23 17:06:44 +0900

    Update Q004.py

--- problems/Q004.py
+++ problems/Q004.py
@@ -1,30 +1,25 @@
 # Q11660
-
 import sys
 
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
-li1 = []
+A = [[0] * (n+1)]
+S = [[0] * (n+1) for _ in range(n+1)]
 
+# 입력값 배열 생성(x번째 = index : x)
 for _ in range(n):
-    li1.append(list(map(int, input().split())))
+    A_row = [0] + list(map(int, input().split()))
+    A.append(A_row)
 
-li1_sum = [[0] for _ in range(n+1)]
+# 합 배열 계산
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        S[i][j] = S[i-1][j] + S[i][j-1] - S[i-1][j-1] + A[i][j]
 
-for i in range(n):
-    for j in range(n):
-        li1_sum[i+1].append(li1_sum[i+1][-1] + li1[i][j])
-
-sum1 = 0
-sum2 = 0
+# 구간 합 배열 계산
 for _ in range(m):
     x1, y1, x2, y2 = map(int, input().split())
-    result = 0
-    for idx1 in range(x1, x2+1):
-        sum1 = li1_sum[idx1][y2]
-        sum2 = li1_sum[idx1][y1-1]
-        result += (sum1 - sum2)
-
+    result = S[x2][y2] - S[x1-1][y2] - S[x2][y1-1] + S[x1-1][y1-1]
     print(result)
